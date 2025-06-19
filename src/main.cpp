aed0e188fa5c495cfec6a26be904a3ba2be51e45
Kishore-Venkatesh <vkish2708@gmail.com>
2025-06-19 18:17:32 +0530

    Added condition check before printing the data

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,13 @@
 int main()
 {
     std::unique_ptr<trainmanager::TrainManagementSystem> trainManager= std::make_unique<trainmanager::TrainManagementSystem>();
-    trainManager->getTrainDetails();
-    trainManager->printTrainList();
+    if (trainManager->getTrainDetails())
+    {
+        trainManager->printTrainList();
+    }
+    else
+    {
+        std::cout << "Unable to  get the train details." << std::endl;
+    }
     return 0;
 }
